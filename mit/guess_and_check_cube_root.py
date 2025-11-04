600e601cde7d0fc55f0606e8a455556375f6f0a5
Silvia Gatto <silvia.gatto@kairaengineering.com>
2025-11-04 16:07:24 +0100

    Wrote Guess and Check Alg to find if a number is a perfect cube

---
+++ mit/guess_and_check_cube_root.py
@@ -0,0 +1,20 @@
+# Guess and Check algorithm to find if an input number is a perfect square, using also boolean flags.
+
+cube = int(input("Enter a number: "))
+print(f"Let's check if {cube} is a perfect cube.")
+print("Loading...........")
+found = False
+
+for i in range(abs(cube)+1):
+    if i**3 == abs(cube):
+        if cube < 0:       
+            i = -i 
+        print(f"{cube} is a perfect cube and its cube root is {i}.")
+        found = True
+        break
+    elif i**3 > cube:
+        break
+
+if not found:
+    print(f"{cube} is not a perfect cube!")
+
