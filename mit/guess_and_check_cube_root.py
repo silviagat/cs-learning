0d88752435a2a2dbd9355e247c3d6f6cf26ad257
Silvia Gatto <silvia.gatto@kairaengineering.com>
2025-11-04 16:09:20 +0100

    Added While loop Version

--- mit/guess_and_check_cube_root.py
+++ mit/guess_and_check_cube_root.py
@@ -3,18 +3,26 @@
 cube = int(input("Enter a number: "))
 print(f"Let's check if {cube} is a perfect cube.")
 print("Loading...........")
-found = False
 
-for i in range(abs(cube)+1):
-    if i**3 == abs(cube):
-        if cube < 0:       
-            i = -i 
-        print(f"{cube} is a perfect cube and its cube root is {i}.")
-        found = True
-        break
-    elif i**3 > cube:
-        break
+i = 1
+while i**3 < N:
+    i += 1
+if i**3 == N:
+    print(i)
+else:
+    print('error')
 
-if not found:
-    print(f"{cube} is not a perfect cube!")
+# found = False
+# for i in range(abs(cube)+1):
+#     if i**3 == abs(cube):
+#         if cube < 0:       
+#             i = -i 
+#         print(f"{cube} is a perfect cube and its cube root is {i}.")
+#         found = True
+#         break
+#     elif i**3 > cube:
+#         break
+
+# if not found:
+#     print(f"{cube} is not a perfect cube!")
 
